f8a0d9ea885ee8a7055c6bddef8ca9b6c2a7154b
Manasa Dade <manasarao_218@yahoo.com>
2020-02-20 14:32:12 +0530

    Added Feature Layaway file

--- xsrc/Layaway.java
+++ xsrc/Layaway.java
@@ -4,6 +4,7 @@ public class Layaway
 {
     public static void main(String args[])
     {
+	System.out.println("Manasa Layaway Application");
         System.out.println("Layaway Application");
     }
 }
