f1b6079b3b4989b892fcc3c4d55a5d542ab84d71
Manasa Dade <manasarao_218@yahoo.com>
2020-02-20 14:34:27 +0530

    Added Feature Ledger Program file

--- client/LedgerProgram.java
+++ client/LedgerProgram.java
@@ -4,6 +4,7 @@ public class LedgerProgram
 {
     public static void main(String args[])
     {
+        System.out.println("Manasa Ledger Program");
         System.out.println("Ledger Program");
     }
 }
